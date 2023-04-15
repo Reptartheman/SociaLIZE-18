49e853f610ce74ddbb222479ed179656d354d76c
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-04-15 11:50:38 -0400

    Models
    Models are all setup

--- models/Reaction.js
+++ models/Reaction.js
@@ -31,3 +31,21 @@ const reactionSchema = new Schema(
   
   
   module.exports = reactionSchema;
+
+//  {#46b,16}
+/* 
+    Line 1: Grabbing our Schema class to setup a reactionSchema
+    from the mongoose module.
+    line 2: Getting the dateFormat file to be used in the getter function
+    on line 21.
+    Line 3: Starts the new schema.
+
+    Line 5: We are setting up the first argument of our callback function
+      - reactionId is given the reaction that a user posts a specific ID.
+    Line 9: Is setting up the reaction body.
+      - It needs to be a string, it is required, and cannot exceed 280 characters.
+    Line 14: we need the username, it must be a string.
+    Line 18: we are setting up when the reaction was posted.
+    Line 25: Is where the second argument starts. Getter functions will appear in JSON format.
+      - Will exclude the id from the formatted JSON.
+*/
--- models/Thought.js
+++ models/Thought.js
@@ -14,6 +14,7 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      //  {#5a4,1} CHECK THIS
       get: timestamp => dateFormat(timestamp)
     },
     username: {
@@ -37,5 +38,39 @@ thoughtSchema.virtual('reactionCount').get(function () {
 
 const Thought = model('Thought', thoughtSchema);
 module.exports = Thought;
+
+
+
+//  {#502,30}
+/* 
+    The first three lines are simply importing our...
+        - Schema and model classes from the mongoose module.
+        - Our reactionSchema.
+        - PENDING: our date format.
+    Starting at line 6, we start to define our schema for our
+    Thought model.
+        - Line 8: Is defining the structure of the "thought"
+        - Line 14: Is saying what time the "thought" was left.
+        - Line 20: Is defining the username and saying it 
+        needs to be there.
+        - Line 24: Is an array of objects from our reactionSchema.
+        - Line 27: Means that documents from the collection will 
+        be converted to JSON. Getters should be included in the JSON
+        representation.
+
+    Line 35: A virtual property simply allows you to make a new
+    property that does not get saved to the database.
+        - An example would be if you have a firstName and lastName
+        property and you want to turn it into a fullName property.
+        - Line 35 is going to save a reaction count that is associated
+        with the specific thought.
+
+    Line 39: Is creating our Thought model.
+        - The model object gives us a way to interact with a
+        specific collection.
+        - A model is a representation of a mongoDB collection.
+        - In this case we have a collection of Thoughts. The collection
+        contains documents which have the data.
+*/
 
 
